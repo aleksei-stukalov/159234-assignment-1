616a3db2bd669fb38284cbd5ec254871a2f9c510
Aleksei Stukalov <stukalov.nz@gmail.com>
2025-03-28 23:49:39 +1100

    add displayInfo method

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,15 @@
 // https://github.com/aleksei-stukalov/159234-assignment-1
 
 public class Main {
+    private static void displayInfo() {
+        System.out.println("**************************************");
+        System.out.println("Assignment 1, 159.234 Semester 1 2025");
+        System.out.println("Submitted by: Aleksei Stukalov, ID: 25016435");
+        System.out.println("My Major at Massey: Software Engineering");
+        System.out.println("**************************************");
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        displayInfo();
     }
 }
