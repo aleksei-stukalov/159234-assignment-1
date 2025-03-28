6e04ac14095eb046d1219d0217c524089651c384
Aleksei Stukalov <stukalov.nz@gmail.com>
2025-03-28 23:49:50 +1100

    add constructor

--- src/School.java
+++ src/School.java
@@ -7,4 +7,17 @@ public class School {
     private ArrayList<Paper> papers;
     private ArrayList<Offering> offerings;
     private ArrayList<Lecturer> lecturers;
+
+    public School(
+            String name,
+            ArrayList<Major> majors,
+            ArrayList<Paper> papers,
+            ArrayList<Offering> offerings,
+            ArrayList<Lecturer> lecturers) {
+        this.name = name;
+        this.majors = majors;
+        this.papers = papers;
+        this.offerings = offerings;
+        this.lecturers = lecturers;
+    }
 }
